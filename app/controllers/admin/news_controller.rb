a2262f30592d2480a65d77a8e07764090457d30c
gataniel <gataniel@gmail.com>
2015-09-10 10:03:58 +0300

    Add news controller

--- app/controllers/admin/news_controller.rb
+++ app/controllers/admin/news_controller.rb
@@ -1,5 +1,40 @@
 class Admin::NewsController < Admin::ApplicationController
+  before_action :find_new, only: %i(edit update destroy)
+
   def index
     @news = New.all
+  end
+
+  def new
+    @new = New.new
+  end
+
+  def create
+    @new = New.new(new_params)
+    if @new.save
+      redirect_to action: :index, notice: 'New successfully created'
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def new_params
+    params.require(:new).permit(:title, :description)
+  end
+
+  def find_new
+    @new = New.find(params[:id])
   end
 end
