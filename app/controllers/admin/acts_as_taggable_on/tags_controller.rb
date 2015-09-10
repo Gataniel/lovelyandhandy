038ed4a27f2cf1ee84abb738d57be12ff7884561
gataniel <gataniel@gmail.com>
2015-09-10 18:17:09 +0300

    Add tags to admin

---
+++ app/controllers/admin/acts_as_taggable_on/tags_controller.rb
@@ -0,0 +1,44 @@
+class Admin::ActsAsTaggableOn::TagsController < Admin::ApplicationController
+  before_action :find_tag, only: %i(edit update destroy)
+
+  def index
+    respond_to do |format|
+      format.html do
+        @tags = ActsAsTaggableOn::Tag.all
+      end
+
+      format.json do
+        tags = ActsAsTaggableOn::Tag.where('name like ?', params[:query][:term]).pluck(:name)
+        render json: tags
+      end
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @tag.update(tag_params)
+      redirect_to action: :index, notice: 'Tag successfully updated'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @tag.destroy
+      redirect_to action: :index, notice: 'Tag successfully destroyed'
+    else
+      redirect_to action: :index, notice: 'Something whent wrong'
+    end
+  end
+
+  private
+
+  def tag_params
+    params.require(:acts_as_taggable_on_tag).permit!
+  end
+
+  def find_tag
+    @tag = ActsAsTaggableOn::Tag.find(params[:id])
+  end
+end
